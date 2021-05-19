f04d56b84a15205a2442546d6cd086a5377be8d4
ytakasugi <sh7.tibi0129@gmail.com>
2021-05-19 23:07:28 +0900

    new cargo project

---
+++ basic/chapter07/chapter0711/chapter0711_02/target/rls/debug/deps/chapter0711_02-81136e96667f88a8.d
@@ -0,0 +1,5 @@
+/home/ytakasugi/005_introduction_to_rust/basic/chapter07/chapter0711/chapter0711_02/target/rls/debug/deps/chapter0711_02-81136e96667f88a8.rmeta: src/main.rs
+
+/home/ytakasugi/005_introduction_to_rust/basic/chapter07/chapter0711/chapter0711_02/target/rls/debug/deps/chapter0711_02-81136e96667f88a8.d: src/main.rs
+
+src/main.rs:
